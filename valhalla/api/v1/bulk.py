cd9367842e5997968fc4f5b8ffc1929dd77b15b9
Matthew Messinger <mattmess1221@gmail.com>
2026-09-16 13:00:53 -0400

    Add a ratelimit to the bulk_textures endpoint

--- valhalla/api/v1/bulk.py
+++ valhalla/api/v1/bulk.py
@@ -1,18 +1,28 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
-
-from valhalla.api.v1.utils import get_textures_url
+from fastapi import APIRouter, Depends, Request
 
 from ...crud import CRUD
+from ...limit import limiter
 from ...schemas import BulkRequest, BulkResponse
 from .user import get_user_textures
+from .utils import get_textures_url
 
 router = APIRouter(tags=["User information"])
 
 
 @router.post("/bulk_textures")
+@limiter.shared_limit(
+    "10/minute",
+    scope="user",
+    error_message=(
+        "You have surpassed the request limit for this endpoint of 10 requests per"
+        " minute. Please don't try to scrape skins. Open an issue if you require"
+        " scraping."
+    ),
+)
 async def bulk_request_textures(
+    request: Request,
     body: BulkRequest,
     crud: Annotated[CRUD, Depends()],
     textures_url: str = Depends(get_textures_url),
