2c742900abb7f7c4d417084c4907cc9da802828d
Matthew Messinger <mattmess1221@gmail.com>
2026-09-16 13:43:26 -0400

    Add a length requirement to bulk_textures uuids field
    This is a breaking change and will require the hdskins client to be
    updated.

--- valhalla/crud.py
+++ valhalla/crud.py
@@ -2,7 +2,7 @@
 # mypy has it disabled in pyproject.toml
 # pyright: reportGeneralTypeIssues=false
 from collections import defaultdict
-from collections.abc import AsyncIterator
+from collections.abc import AsyncIterator, Iterable
 from dataclasses import dataclass
 from datetime import UTC, datetime
 from typing import Annotated
@@ -43,7 +43,7 @@ class CRUD:
         )
         return result.scalar()
 
-    async def resolve_uuids(self, uuids: list[UUID]) -> AsyncIterator[models.User]:
+    async def resolve_uuids(self, uuids: Iterable[UUID]) -> AsyncIterator[models.User]:
         for uid in uuids:
             usr = await self.get_user_by_uuid(uid)
             if usr:
